a3e2c204c6fad65ee848516c79cc6c6aaef7ecff
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2015-02-03 08:14:34 -0700

    Fixed missing fonts

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,6 @@ Bundler.require(*Rails.groups)
 module Tapor3
   class Application < Rails::Application
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
-    config.assets.precompile += ["fontawesome-webfont.ttf",
-                                 "fontawesome-webfont.eot",
-                                 "fontawesome-webfont.svg",
-                                 "fontawesome-webfont.woff"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
