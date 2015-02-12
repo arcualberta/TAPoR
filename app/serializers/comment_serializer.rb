6349081d99b7c92de123fcf0a5492f49c431ca73
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2015-02-12 15:01:51 -0700

    Compromise change on where data is gathered. this will change once we redefine tool serialization.

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,4 +1,11 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :content, :is_pinned, :is_hidden, :index
+  attributes :id, :content, :is_pinned, :is_hidden, :index, :tool
   has_one :user  
+
+  def tool
+  	result = {
+  		id: object.tool[:id],
+  		name: object.tool[:name],
+  	}
+  end
 end
