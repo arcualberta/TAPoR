7e069f5eccfb77367ce29cb46c1e38120e11bb3c
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2015-04-13 10:47:42 -0600

    Minor changes

--- app/serializers/tool_serializer.rb
+++ app/serializers/tool_serializer.rb
@@ -1,8 +1,8 @@
 class ToolSerializer < ActiveModel::Serializer
-	attributes :id, :user_id, :name, :description, :is_approved, :image_url, :creators_name, :creators_email, :creators_url, :star_average, :thumb_url, :url, :last_updated, :nature, :language, :code
+	attributes :id, :user_id, :name, :description, :is_approved, :image_url, :creators_name, :creators_email, :creators_url, :star_average, :url, :last_updated, :nature, :language, :code, :thumb_url
 
   def thumb_url
-    return object.image_url ? object.image_url.gsub(/\.png/, "-thumb.png") : "" ;   
+    return object.image_url ? object.image_url.gsub(/\.png/, "-thumb.png") : "/images/tools/missing-thumb.png" ;   
   end
 
   def description
