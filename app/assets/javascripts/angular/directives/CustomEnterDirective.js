d68156ae6528d8ac491c0cbb504816c5d9281a97
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2015-10-14 10:32:52 -0600

    Fixes #7 .

---
+++ app/assets/javascripts/angular/directives/CustomEnterDirective.js
@@ -0,0 +1,13 @@
+app.directive('customEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.customEnter);
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+});
