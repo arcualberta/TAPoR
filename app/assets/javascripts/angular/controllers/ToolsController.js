4cd5ace4b56eb13635cdca57a179425c738cbc60
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2015-11-09 15:20:32 -0700

    Added tool view tag cloud.

--- app/assets/javascripts/angular/controllers/ToolsController.js
+++ app/assets/javascripts/angular/controllers/ToolsController.js
@@ -9,6 +9,7 @@ app.controller('ToolsViewController', ['$scope', '$http', '$location', '$routePa
 	$scope.named_id = $routeParams.named_id;
 	$scope.data = {};  
   $scope.data.newComment = "";
+  $scope.data.tagsLoaded = false;
 
 	$scope.tinymceOptions = {
   	menubar : false,
@@ -43,6 +44,7 @@ app.controller('ToolsViewController', ['$scope', '$http', '$location', '$routePa
 			services.tool.get_tags($scope.id).then(
 				function(data){
 					$scope.data.tags = data;
+          $scope.data.tagsLoaded = true;
 				},
 				function(errorMesssage){
 					$scope.error = errorMesssage
